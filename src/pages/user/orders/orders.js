5d76835ab56cb20eae121052f1f23c1e355791e5
ahmiadn <ahmiadn@hilti.com>
2021-07-14 14:50:50 +0530

    add new changes

---
+++ src/pages/user/orders/orders.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { FlatList, TouchableOpacity, TextInput , ScrollView, SafeAreaView, Text, View, Image } from 'react-native';
+import styles from './styles';
+import BackButton from '../../../components/BackButton/BackButton';
+import { Restaurant } from '../../../data/dataArrays';
+import OcticonsIcon from 'react-native-vector-icons/Octicons';
+const dot = <OcticonsIcon name="primitive-dot" size={10} color="#6B6B6B"  />;
+
+
+export default class UserOrders extends React.Component {
+
+ 
+  constructor(props) {
+    super(props);
+    this.state={
+        status:false,
+    }
+   
+  
+  }
+
+  ShowDiv(){
+       this.setState({
+        status:true
+       })   
+  }
+
+
+
+  
+
+  renderRestaurants = ({ item, index }) => (
+    <TouchableOpacity  underlayColor='rgba(73,182,77,0.9)'  >
+      <View>
+          <View style={styles.listItem}>
+                <View>
+                    <Text style={styles.title1}>{item.title}</Text>
+                    <Text style={styles.date}> 31 May 2020, 07:55 PM &nbsp;{dot}&nbsp; $43.95</Text>
+                </View>
+            <View>
+               {index ==2 ? <Text style={styles.btnCanl}>Cancelled</Text> : <Text style={styles.btnDelv}>Delivered</Text>}
+            </View> 
+          </View>  
+
+        <View style={{flex:1,}}>
+
+        {index ==0 ?
+        <TouchableOpacity  underlayColor='rgba(73,182,77,0.9)' onPress={(e)=>this.ShowDiv()} >
+            <Image source={require("../../../../assets/icons/Collapse.png")}   style={{position:"absolute",bottom:4}}/>
+          </TouchableOpacity>  
+
+          :<TouchableOpacity  underlayColor='rgba(73,182,77,0.9)' onPress={(e)=>this.ShowDiv()} >
+          <Image source={require("../../../../assets/icons/CollapseClosed.png")}   style={{position:"absolute",bottom:4}}/>
+        </TouchableOpacity>  }
+        </View>
+
+        {index ==0 ?
+        <View>
+       
+        <View style={styles.container1}>
+                <Text style={[styles.text]}>1 X{item.title}</Text>
+                <Text style={[styles.text]}>${item.price}</Text>
+         </View>
+
+         <View style={styles.container1}>
+                <Text style={[styles.text ]}>1 X{item.title}</Text>
+                <Text style={[styles.text]}>${item.price}</Text>
+         </View>
+
+         <View style={styles.container1}>
+                <Text style={styles.text}> 1 X{item.title}</Text>
+                <Text style={[styles.text]}>${item.price}</Text>
+         </View>
+
+         <TouchableOpacity >
+                <View style={styles.btnClickContain_right}>
+                    <Text style={styles.btnText}>REORDER </Text>
+                </View>
+            </TouchableOpacity>
+         </View>  
+        : <Text ></Text>} 
+
+    </View>
+    </TouchableOpacity >
+  );
+
+  
+
+
+
+  render() {
+
+     const {value} = this.state;
+
+    return (
+
+      <SafeAreaView style={{marginBottom:80}}>
+          <BackButton backIcon={true} onPress={() => {this.props.navigation.goBack();}}/>
+          <Text style={styles.title}>Orders</Text>
+        <ScrollView>
+
+        <View style={styles.container}>
+                    <FlatList
+                        style={{flex:1}}
+                        data={Restaurant}
+                        renderItem={this.renderRestaurants}
+                        keyExtractor={item => item.recipeId}
+                    />
+        </View>
+          
+        </ScrollView>
+           
+      </SafeAreaView>
+    );
+  }
+}
+
---
+++ src/pages/user/orders/styles.js
@@ -0,0 +1,103 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  
+  title: {
+    marginLeft: 15,
+    marginBottom:0,
+    marginTop:20,
+    color: "#000",
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    position:'absolute',
+    top:0,
+    right: 0,
+    left:0
+    
+  },
+
+  title1:{
+    fontWeight:"bold",
+    fontSize:20
+  },
+
+
+
+
+
+container: {
+  flex: 1,
+  marginTop:10,
+  
+},
+listItem:{
+  margin:10,
+  padding:20,
+  width:"100%",
+  height:150,
+  flex:1,
+  alignSelf:"center",
+  flexDirection:"row",
+
+  justifyContent:"space-between",
+  backgroundColor:"#fff"
+},
+
+container1: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    //borderWidth:.2,
+    //borderBottomColor:"#C5C6C8"
+  },
+
+  btnClickContain_right: {
+    width:"92%",
+    marginRight :10,
+    backgroundColor: '#F0F0F0',
+    marginLeft:15,
+    marginRight:15,
+    height:60,
+    borderRadius:15,
+    padding:0 ,
+    alignItems:"center" ,
+    justifyContent: 'center'
+  },
+
+  text: {
+    marginLeft: 10,
+    fontSize: 16,
+  },
+
+date:{
+    marginTop:20,
+    marginLeft:0
+},
+
+btnDelv:{
+    backgroundColor:"#88F79A",
+    height:30,
+    width:80,
+    padding:4,
+    borderRadius:5,
+    textAlign:"center"
+    
+},
+
+btnCanl:{
+    backgroundColor:"#FC8B74",
+    height:30,
+    width:80,
+    padding:4,
+    borderRadius:5,
+    textAlign:"center"
+    
+}
+
+ 
+});
+
+export default styles;
